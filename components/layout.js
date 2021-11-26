48890980cd01fe2339da8d31ee1e09fd7299cba1
Siddharth <ahoy@barelyhuman.dev>
2021-11-26 09:51:04 +0530

    fix layout styles

--- components/layout.js
+++ components/layout.js
@@ -5,9 +5,9 @@ import { Header } from "./header";
 export function Layout({ children, className, ...props }) {
   return (
     <>
-      <div className="min-h-screen container p-4 mx-auto">
+      <div className="h-full container p-4 mx-auto">
         <Header />
-        <Box marginT-8 className={`${className}`} {...props}>
+        <Box marginT-8 className={`h-4/5 ${className}`} {...props}>
           {children}
         </Box>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import Box from "components/box";
-import { Credits } from "components/credits";
 import { Layout } from "components/layout";
 import UsernameInputForm from "containers/username-input";
 
 export default function Home() {
   return (
     <>
-      <Layout className="min-h-screen flex flex-col items-end justify-center">
+      <Layout className="flex flex-col items-end justify-center">
         <UsernameInputForm marginB-16 autoFocus={true} />
       </Layout>
     </>
